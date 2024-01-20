a02c22f82f3609a3105d9f749869092f2f21cff4
Tyler M. Kontra <tyler@tylerkontra.com>
2024-01-20 09:25:12 -0800

    accepts name input from argument, does not change the interface of main function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
+import sys
 
 
 def main():
-    name = "World"
+    name = os.getenv("DEMO_NAME") or "World"
     print("Starting: " + str(os.getuid()))
     print("Hello " + name + "!")
 
 
 if __name__ == "__main__":
+    name = sys.argv[1]
+    os.environ["DEMO_NAME"] = name
     main()
